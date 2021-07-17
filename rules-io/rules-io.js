66909c705ac791408cd11f58c54e8063dfccbe7c
hjboehle <hj@boehle.info>
2021-07-17 17:03:01 +0200

    add comments

--- rules-io/rules-io.js
+++ rules-io/rules-io.js
@@ -6,24 +6,28 @@ module.exports = function (RED) {
         var nodeContext = this.context();
         node.on('input', function (msg, send, done) {
             var rulesJson = nodeContext.get("rulesJson_" + node.id) || null;
+            // are there no rules stored in context rules, will the rules reading from the rules file and storing in context
             if (rulesJson === null) {
-                node.log("read rules from file and store in context: " + "rulesJson_" + node.id);
                 rulesJson = require(this.rulesfilename);
                 nodeContext.set("rulesJson_" + node.id, rulesJson);
+                // call the function to create and store the status information in context
                 statusJson = createStatus(rulesJson)
             }
 
+            // only for backward compability to Node-Red 0.x
             send = send || function () { node.send.apply(node, arguments) }
+
             msg.payload = rulesJson;
             node.log("before send");
             send(msg);
 
+            // only for backward compability to Node-Red 0.x
             if (done) {
                 done();
             }
         });
+        // function to create and store the status information in context
         function createStatus(rulesJson) {
-            node.log("function addStatus");
             statusJson = rulesJson.config;
             for (var i = 0; i < statusJson.input.length; i++) {
                 statusJson.input[i].value = statusJson.input[i].inititalValue;
@@ -34,7 +38,6 @@ module.exports = function (RED) {
                 delete statusJson.output[i].inititalValue;
             }
             nodeContext.set("statusJson_" + node.id, statusJson);
-            node.log("statusJson: " + JSON.stringify(statusJson));
             return statusJson;
         }
     }
