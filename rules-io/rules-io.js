e2779b7c65a18891752ecee0d51396699d024117
hjboehle <hj@boehle.info>
2021-08-07 19:22:48 +0200

    version 0.0.1 ready to review and test
    the functions for the version 1 are implemented.
    
    The node read the rulesfile an store the rule in the context oft the node. Whenn input topics and payloads comes to on of the node inputs, the output of the node sends the right topic and payload.

--- rules-io/rules-io.js
+++ rules-io/rules-io.js
@@ -6,54 +6,37 @@ module.exports = function (RED) {
         this.outputs = config.outputs || 1;
         var nodeContext = this.context();
         node.on('input', function (msg, send, done) {
-            var rulesJson = nodeContext.get("rulesJson_" + node.id) || null;
+            var rules = nodeContext.get("rules_" + node.id) || null;
             // are there no rules stored in context rules, will the rules reading from the rules file and storing in context
-            if (rulesJson === null) {
-                rulesJson = require(this.rulesfilename);
-                nodeContext.set("rulesJson_" + node.id, rulesJson);
+            if (rules === null) {
+                rules = require(this.rulesfilename);
+                nodeContext.set("rules_" + node.id, rules);
                 // call the function to create and store the status information in context
-                statusJson = createStatus(rulesJson)
+                status = rules.config;
             }
 
             // adjustment of the status due to the input parameters
-            statusJson = adjustStatus(statusJson, msg.topic, msg.payload, rulesJson.rules);
-            node.log("status: " + JSON.stringify(statusJson) + "outputMessageList: " + getOutputMessageList(statusJson));
-            nodeContext.set("statusJson_" + node.id, statusJson);
+            status = adjustStatus(status, msg.topic, msg.payload, rules.rules);
+            node.log("status: " + JSON.stringify(status) + ", outputMessages: " + JSON.stringify(status.output));
+            nodeContext.set("status_" + node.id, status);
 
-            // only for backward compability to Node-Red 0.x
+            // only for backward compability to Node-RED 0.x
             send = send || function () { node.send.apply(node, arguments) }
 
             // send rule output to node output
-            msg.payload = getOutputMessageList(statusJson);
-            node.send(msg);
-            //var msg1 = "1";
-            //var msg2 = "2";
-            //send([ msg1, msg2 ]);
+            node.send(status.output);
 
-            // only for backward compability to Node-Red 0.x
+            // only for backward compability to Node-RED 0.x
             if (done) {
                 done();
             }
         });
-        // function to create and store the status information in context
-        function createStatus(rulesJson) {
-            statusJson = rulesJson.config;
-            for (var i = 0; i < statusJson.input.length; i++) {
-                statusJson.input[i].value = statusJson.input[i].initialValue;
-                delete statusJson.input[i].initialValue;
-            }
-            for (var i = 0; i < statusJson.output.length; i++) {
-                statusJson.output[i].value = statusJson.output[i].initialValue;
-                delete statusJson.output[i].initialValue;
-            }
-            return statusJson;
-        }
 
         // function to adjust the status with the input and output parameters
-        function adjustStatus(status, topic, value, rules) {
+        function adjustStatus(status, topic, payload, rules) {
             for (var i = 0; i < status.input.length; i++) {
                 if (topic === status.input[i].topic) {
-                    status.input[i].value = value;
+                    status.input[i].payload = payload;
                 }
             }
             for (var i = 0; i < rules.length; i++) {
@@ -62,13 +45,6 @@ module.exports = function (RED) {
                 }
             }
             return status;
-        }
-        function getOutputMessageList(status) {
-            var outputMessageList = [];
-            for (var i = 0; i < status.output.length; i++) {
-                outputMessageList[i] = status.output[i].value;
-            }
-            return outputMessageList;
         }
     }
     RED.nodes.registerType("rules-io", RulesIoNode);
